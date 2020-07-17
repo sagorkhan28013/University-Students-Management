aa67400f7d7a1b209990ae382b174dc03d94c65d
SAGOR-KHAN <50752296+sagorkhan28013@users.noreply.github.com>
2020-07-18 01:31:18 +0600

    University Students Management

---
+++ Home.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace School_Student_Management
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Window
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void btn(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_logout(object sender, RoutedEventArgs e)
+        {
+            
+            MessageBoxResult result = MessageBox.Show("Do You Want To Exit", "Exit", MessageBoxButton.YesNo);
+
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    System.Environment.Exit(0);
+                    break;
+                case MessageBoxResult.No:
+                    MessageBox.Show("Thank you!", "Exit");
+                    break;
+            }
+        }
+
+        private void btn_admission(object sender, RoutedEventArgs e)
+        {
+            admission ad = new admission();
+            ad.Show();
+            this.Close();
+        }
+
+        private void btn_update(object sender, RoutedEventArgs e)
+        {
+            update up = new update();
+            up.Show();
+            this.Close();
+        }
+
+        private void btn_delete(object sender, RoutedEventArgs e)
+        {
+            delete de = new delete();
+            de.Show();
+            this.Close();
+        }
+
+        private void btn_search(object sender, RoutedEventArgs e)
+        {
+            Student_Details sd = new Student_Details();
+            sd.Show();
+            this.Close();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace School_Student_Management
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_exit_click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result= MessageBox.Show("Do You Want To Exit", "Exit", MessageBoxButton.YesNo);
+
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    System.Environment.Exit(0);
+                    break;
+                case MessageBoxResult.No:
+                    MessageBox.Show("Thank you!", "Exit");
+                    break;
+            }
+            
+        }
+
+        private void btn_login_click(object sender, RoutedEventArgs e)
+        {
+            Home bw = new Home();
+            if(username.Text=="nwu" && password.Password=="1234")
+            {
+                bw.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Dont Match", "Login Status", MessageBoxButton.OKCancel);
+                username.Text = "";
+                password.Password = "";
+            }
+        }
+
+        private void btn_student(object sender, RoutedEventArgs e)
+        {
+            Student_Details sd = new Student_Details();
+            sd.Show();
+            this.Close();
+        }
+
+        private void username_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
